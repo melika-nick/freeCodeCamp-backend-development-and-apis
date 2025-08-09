77123e9e6c3a78b1c133e0bd3a9216f4a07c2afc
melika-nick <nikmelika8@gmail.com>
2025-08-09 17:02:31 +0330

    come back to first version

--- 01-timestamp-microservice/index.js
+++ 01-timestamp-microservice/index.js
@@ -2,29 +2,52 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
+// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
 });
 
-// listen for requests :)
+app.get("/api/:date?", (req, res) =>
+{
+  let dateStr = req.params.date;
+  let date;
+  if (!dateStr){
+    date = new Date();
+  } else {
+    if (/^\d+$/.test(dateStr)){
+      date = new Date(parseInt(dateStr));
+    } else {
+      date = new Date(dateStr);
+    }
+  }
+  if (date.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  }
+});
+
+// Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
