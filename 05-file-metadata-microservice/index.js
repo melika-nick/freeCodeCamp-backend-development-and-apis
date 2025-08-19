191f86098e1fd18f38e437ec5c06880addf671f7
melika-nick <nikmelika8@gmail.com>
2025-08-19 09:27:30 +0330

    add '/api/fileanalyse' endpoint and it's function

--- 05-file-metadata-microservice/index.js
+++ 05-file-metadata-microservice/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
 
 var app = express();
 
@@ -11,8 +12,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ storage: multer.memoryStorage() });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.json({ error: 'No file uploaded' });
+  }
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
